85be6a991e6d9402c085ec45001cdd19da9ac352
Samurai-way <olegmoishevych@gmail.com>
2022-08-13 19:09:55 +0300

    Home Work 4 repack =))

--- src/p2-homeworks/h4/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/p2-homeworks/h4/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -1,6 +1,7 @@
 import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes} from 'react'
 import s from './SuperCheckbox.module.css'
 
+
 // тип пропсов обычного инпута
 type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
@@ -21,6 +22,8 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // сделайте так чтоб работал onChange и onChangeChecked
+        if(onChangeChecked) onChangeChecked(e.currentTarget.checked)
+        if(onChange) onChange(e)
     }
 
     const finalInputClassName = `${s.checkbox} ${className ? className : ''}`
